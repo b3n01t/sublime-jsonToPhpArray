0a19eb58060f2bacdaeca6f4120e74e47bab6072
b3n01t <benoit.auvinet@gmail.com>
2014-07-07 14:57:10 -0500

    puts the converted array(s) in a separate file

--- jsonTophpArray.py
+++ jsonTophpArray.py
@@ -11,24 +11,44 @@ class jsonTophpArrayCommand(sublime_plugin.TextCommand):
 		if not braces:
 			new_sels = []
 			for sel in sels:
-				new_sels.append(self.view.find('\}', sel.end()))
+				new_sels.append(self.view.find('\{', sel.end()))
 			sels.clear()
 			for sel in new_sels:
 				sels.add(sel)
 			self.view.run_command("expand_selection", {"to": "brackets"})
-		php = self.convert(sels[0])
-		print php
-		self.view.insert(edit, sel.end(), "\n/*---- PHP Array ----*/\n"+php+"\n")
+		
+		converted = self.convert(sels[0])
+		name = converted[0]+'_array.php'
+		php = converted[1]
+
+		openViews =  self.view.window().views()
+		newView = None
+		for v in openViews:
+			if v.name() == name:
+				newView = v
+				break
+
+
+		newView = newView and newView or self.view.window().new_file()
+		
+		newView.set_syntax_file('Packages/PHP/PHP.tmLanguage')
+		newView.set_name(name)
+		newView.insert(edit, 0, "<?php\n/*---- PHP Array ----*/\n"+php+"\n?>")
+		self.view.window().focus_view(newView)
 
 	def convert(self, json):
 		string = self.view.substr(json)
-		newString = re.sub(r'(^[#/]? *)var ',r'\1$',string)
+		name = re.search(r'var (.*) ?=',string)
+		name = name and name.group(1) or 'PHP_Array'
+		newString = re.sub(r'([#/]? *)var ',r'\1$',string)
 		newString = re.sub(r'= ?{','= array(',newString)
-		newString = re.sub(r'^ ?{','array(',newString)
+		newString = re.sub(r'[ \t]*{','array(',newString)
 		newString = re.sub(r': ?{','=> array(',newString)
 		newString = re.sub(r': ?\[','=> array(',newString)
 		newString = re.sub(r':','=>',newString)
-		newString = re.sub(r'"','\'',newString)
+		newString = re.sub(r'["]','\'',newString)
+		newString = re.sub(r'([ \t\n]*)([^"\'\t ].*[^"\' \t]) ?=>',r"\1'\2'=>",newString)
 		newString = re.sub(r'[}\]]',')',newString)
-		return newString
+		newString = re.sub(r';?$',';',newString)
+		return (name.strip(),newString)
 		

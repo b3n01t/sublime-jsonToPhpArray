f62688c1f259e2193f1bbc421b2e782fe439be0d
b3n01t <benoit.auvinet@gmail.com>
2014-07-08 20:58:21 -0500

    ; at end of varaible declaration. and remove "auto select"

--- jsonTophpArray.py
+++ jsonTophpArray.py
@@ -2,21 +2,7 @@ import sublime, sublime_plugin,re
 
 class jsonTophpArrayCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		braces = False
 		sels = self.view.sel()
-		for sel in sels:
-			if self.view.substr(sel).find('{') != -1:
-				braces = True
-		# self.view.insert(edit, 0, "Hello, World!")
-		if not braces:
-			new_sels = []
-			for sel in sels:
-				new_sels.append(self.view.find('\{', sel.end()))
-			sels.clear()
-			for sel in new_sels:
-				sels.add(sel)
-			self.view.run_command("expand_selection", {"to": "brackets"})
-		
 		converted = self.convert(sels[0])
 		name = converted[0]+'_array.php'
 		php = converted[1]
@@ -28,16 +14,17 @@ class jsonTophpArrayCommand(sublime_plugin.TextCommand):
 				newView = v
 				break
 
-
 		newView = newView and newView or self.view.window().new_file()
 		
 		newView.set_syntax_file('Packages/PHP/PHP.tmLanguage')
 		newView.set_name(name)
-		newView.insert(edit, 0, "<?php\n/*---- PHP Array ----*/\n"+php+"\n?>")
-		self.view.window().focus_view(newView)
+		newView.insert(edit, 0, "<?php\n/*---- PHP Array ----*/\n"+php+"\n\n?>")
+		if self.view.window():
+			self.view.window().focus_view(newView)
 
 	def convert(self, json):
 		string = self.view.substr(json)
+		string = string.strip()
 		name = re.search(r'var (.*) ?=',string)
 		name = name and name.group(1) or 'PHP_Array'
 		newString = re.sub(r'([#/]? *)var ',r'\1$',string)
@@ -49,6 +36,7 @@ class jsonTophpArrayCommand(sublime_plugin.TextCommand):
 		newString = re.sub(r'["]','\'',newString)
 		newString = re.sub(r'([ \t\n]*)([^"\'\t ].*[^"\' \t]) ?=>',r"\1'\2'=>",newString)
 		newString = re.sub(r'[}\]]',')',newString)
-		newString = re.sub(r';?$',';',newString)
+		newString = re.sub(r'\)([ \t\n]*)\$',r");\1$",newString)
+		newString = re.sub(r'\);?$',r");",newString)
 		return (name.strip(),newString)
 		
